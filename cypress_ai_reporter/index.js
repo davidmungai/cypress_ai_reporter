e1bef33fe555fb7fa4d3bb5a07ab711d5ed373c4
davidmungai <david.mungai1@accesscfa.com>
2025-03-31 13:48:20 +0300

    added typescript types

--- cypress_ai_reporter/index.js
+++ cypress_ai_reporter/index.js
@@ -24,27 +24,30 @@ class CypresAiReporter {
     constructor(runner) {
         const stats = runner.stats;
         runner
-            .on(EVENT_RUN_BEGIN, () => {
-            console.log('start ======================');
+            .on(EVENT_RUN_BEGIN, (suite) => {
+            console.log(`EVENT_RUN_BEGIN:${EVENT_RUN_BEGIN}==========`, suite);
         })
-            .on(EVENT_SUITE_BEGIN, () => {
-            console.log(EVENT_SUITE_BEGIN);
+            .on(EVENT_SUITE_BEGIN, (suite) => {
+            console.log(`EVENT_SUITE_BEGIN:${EVENT_SUITE_BEGIN}==========`, suite);
         })
-            .on(EVENT_SUITE_END, () => {
-            console.log(EVENT_SUITE_END);
-            ;
+            .on(EVENT_SUITE_END, (suite) => {
+            console.log(`EVENT_SUITE_END:${EVENT_SUITE_END}==========total number=======`, suite.total());
         })
-            .on(EVENT_TEST_PASS, test => {
+            .on(EVENT_TEST_PASS, (test) => {
             // Test#fullTitle() returns the suite name(s)
             // prepended to the test title
+            console.log(`${EVENT_TEST_PASS}==========`, test);
             console.log(`pass: ${test.fullTitle()}`);
         })
             .on(EVENT_TEST_FAIL, (test, err) => {
+            console.log(`EVENT_TEST_FAIL:${EVENT_TEST_FAIL}==========`, test);
+            console.log(`EVENT_TEST_FAIL:${EVENT_TEST_FAIL}==========`, err);
             console.log(`fail: ${test.fullTitle()} - error: ${err.message}`);
         })
-            .once(EVENT_RUN_END, () => {
+            .once(EVENT_RUN_END, (suite) => {
             // console.log(`end: ${stats.passes}/${stats.passes + stats.failures} ok`);
             console.log(`end: ${EVENT_RUN_END} ${stats === null || stats === void 0 ? void 0 : stats.duration} ok`);
+            console.log(`${EVENT_RUN_END}==========`, suite);
         });
     }
 }
--- cypress_ai_reporter/index.ts
+++ cypress_ai_reporter/index.ts
@@ -33,30 +33,34 @@ class CypresAiReporter{
     const stats =runner.stats 
 
     runner
-      .on(EVENT_RUN_BEGIN, () => {
-        console.log('start ======================');
+      .on(EVENT_RUN_BEGIN, (suite:Mocha.Suite) => {
+        console.log(`EVENT_RUN_BEGIN:${EVENT_RUN_BEGIN}==========`,suite)
       })
-      .on(EVENT_SUITE_BEGIN, () => {
-     console.log(EVENT_SUITE_BEGIN);
-
+      .on(EVENT_SUITE_BEGIN, (suite:Mocha.Suite) => {
+        console.log(`EVENT_SUITE_BEGIN:${EVENT_SUITE_BEGIN}==========`,suite)
       })
-      .on(EVENT_SUITE_END, () => {
-        console.log(EVENT_SUITE_END);
+      .on(EVENT_SUITE_END, (suite:Mocha.Suite) => {
+        console.log(`EVENT_SUITE_END:${EVENT_SUITE_END}==========total number=======`,suite.total())
         ;
       })
-      .on(EVENT_TEST_PASS, test => {
+      .on(EVENT_TEST_PASS, (test:Mocha.Test) => {
         // Test#fullTitle() returns the suite name(s)
         // prepended to the test title
+        console.log(`${EVENT_TEST_PASS}==========`,test)
         console.log(`pass: ${test.fullTitle()}`);
       })
       .on(EVENT_TEST_FAIL, (test, err) => {
+        console.log(`EVENT_TEST_FAIL:${EVENT_TEST_FAIL}==========`,test)
+        console.log(`EVENT_TEST_FAIL:${EVENT_TEST_FAIL}==========`,err)
+
         console.log(
           `fail: ${test.fullTitle()} - error: ${err.message}`
         );
       })
-      .once(EVENT_RUN_END, () => {
+      .once(EVENT_RUN_END, (suite:Mocha.Suite) => {
         // console.log(`end: ${stats.passes}/${stats.passes + stats.failures} ok`);
         console.log(`end: ${EVENT_RUN_END} ${stats?.duration} ok`);
+        console.log(`${EVENT_RUN_END}==========`,suite)
 
       });
   }
