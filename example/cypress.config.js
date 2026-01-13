5516e1eeb001c8fb3148ff98c37cc5bcede74b5d
davidmungai <mungahdaudi@gmail.com>
2026-01-13 10:06:33 +0300

    setup cypress ai reporter

--- example/cypress.config.js
+++ example/cypress.config.js
@@ -1,19 +1,18 @@
 const { defineConfig } = require("cypress");
-const cypress_ai_reporter = require("cypress_ai_reporter");
+const { cypressAiReporter } = require("cypress-ai-reporter");
 
 
 
 module.exports = defineConfig({
   e2e: {
     video: true,
-    reporter: 'cypress_ai_reporter',
-    reporterOptions: {
-      mochaFile: 'results/my-test-output.xml',
-      toConsole: true,
-    },
+    // reporter: 'cypress_ai_reporter',
+    // reporterOptions: {
+    //   mochaFile: 'results/my-test-output.xml',
+    //   toConsole: true,
+    // },
     setupNodeEvents(on, config) {
-      // implement node event listeners here
-   
+      cypressAiReporter(on, config);
     },
   },
 });``
